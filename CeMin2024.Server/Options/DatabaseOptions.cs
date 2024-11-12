8477954ef996e6cbcd582fbd9b9c0dcf0ea787ea
Raul Hernandez <rhernandez@ins.gov.co>
2024-11-11 22:01:41 -0500

    Ajustes en RuntimeIdentifier y dependencias en CeMin2024.Client.csproj, y añadido de nuevas configuraciones de base de datos en CeMin2024.Server

---
+++ CeMin2024.Server/Options/DatabaseOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace CeMin2024.Server.Options
+{
+    public class DatabaseOptions
+    {
+        public int CommandTimeout { get; set; }
+        public int MaxRetryAttempts { get; set; }
+        public bool EnableDetailedErrors { get; set; }
+    }
+}
---
+++ CeMin2024.Server/Options/OptionsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CeMin2024.Server.Options
+{
+    public static class OptionsConfiguration
+    {
+        public static IServiceCollection AddServerOptions(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<CorsOptions>(
+                configuration.GetSection("Cors"));
+
+            services.Configure<DatabaseOptions>(
+                configuration.GetSection("DatabaseOptions"));
+
+            return services;
+        }
+    }
+}
